31970b3d474db1b441d286706c9bf6d992809761
Alexey Ryumin <aryumin@aryumin.ru>
2022-10-10 21:28:38 +0300

    added expr base

---
+++ MakeLox/Expr.cs
@@ -0,0 +1,19 @@
+using ru.aryumin.Lox;
+
+public abstract class Expr {
+    
+}
+
+public class Binary : Expr {
+
+    public Expr Left { get; init; }
+    public Token Operator { get; set; }
+    public Expr Right { get; init; }
+
+    public Binary(Expr left, Token @operator, Expr right)
+    {
+        Left = left;
+        Operator = @operator;
+        Right = right;
+    }
+}
