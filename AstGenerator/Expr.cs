70fa2acc29acead3702ae655bee755ed5908e7fa
Алексей Рюмин <aryumin@aryumin.ru>
2022-10-13 22:03:56 +0300

    fixed generator ast

---
+++ AstGenerator/Expr.cs
@@ -0,0 +1,45 @@
+using System;
+using ru.aryumin.Lox;
+namespace ru.aryumin.Lox {
+
+	public abstract class Expr {
+	}
+	public class Binary : Expr {
+
+		public Expr Left {get; set;}
+		public Token Operator {get; set;}
+		public Expr Right {get; set;}
+		public Binary (Expr left, Token @operator, Expr right) {
+			Left = left;
+			Operator = @operator;
+			Right = right;
+		}
+	}
+
+	public class Grouping : Expr {
+
+		public Expr Expression {get; set;}
+		public Grouping (Expr expression) {
+			Expression = expression;
+		}
+	}
+
+	public class Literal : Expr {
+
+		public object Value {get; set;}
+		public Literal (object value) {
+			Value = value;
+		}
+	}
+
+	public class Unary : Expr {
+
+		public Token Operator {get; set;}
+		public Expr Right {get; set;}
+		public Unary (Token @operator, Expr right) {
+			Operator = @operator;
+			Right = right;
+		}
+	}
+
+}
--- AstGenerator/Program.cs
+++ AstGenerator/Program.cs
@@ -8,51 +8,61 @@ public static class Program {
         }
         var outputDir = args.First();
         DefineAst(outputDir, "Expr", new string[]{
-            "Binary : Expr left, Token operator, Expr right",
+            "Binary : Expr left, Token @operator, Expr right",
             "Grouping : Expr expression",
             "Literal : object value",
-            "Unary : Token operator, Expr right"
+            "Unary : Token @operator, Expr right"
         });
     }
 
     private static void DefineAst(string outputDir, string baseName, IEnumerable<string> types){
         var path = $"{outputDir}/{baseName}.cs";
-        if(File.Exists(path)) return;
+        if(File.Exists(path)) File.Delete(path);
 
-        File.Create(path);
         var sb = new StringBuilder();
+        sb.AppendLine("using System;");
         sb.AppendLine("using ru.aryumin.Lox;");
+        sb.AppendLine("namespace ru.aryumin.Lox {");
         sb.AppendLine();
-        sb.AppendLine($"abstract class {baseName} {{");
+        sb.AppendLine($"\tpublic abstract class {baseName} {{");
+        sb.AppendLine("\t}");
 
         foreach(var type in types){
             var className = type.Split(':')[0].Trim();
             var fields = type.Split(':')[1].Trim();
             DefineType(sb, baseName, className, fields);
         }
-
+        
         sb.AppendLine("}");
+        File.WriteAllText(path, sb.ToString());
     }
 
     private static void DefineType(StringBuilder sb, string baseName, string className, string fieldsList){
-        sb.AppendLine($"\tpublic class {className} : {baseName} {{");
-        sb.AppendLine($"\t\t{className} ({fieldsList} {{");
         var fields = fieldsList.Split(',');
+        sb.AppendLine($"\tpublic class {className} : {baseName} {{");
 
         // properties
         sb.AppendLine();
         foreach(var f in fields){
-            (string fType, string fName) prop = 
-                f.Split(' ', StringSplitOptions.RemoveEmptyEntries).Select(f => (f[0], f[1])); //error
-            prop.fName = char.ToUpper(prop.fName[0]) + prop.fName.Substring(1);
-            sb.AppendLine($"\tpublic {prop.fType} {prop.fName} {{get; set;}}");
+            var propData = f.Split(' ', StringSplitOptions.RemoveEmptyEntries); //error
+            (string fType, string fName) prop = (propData[0], propData[1]);
+            prop.fName = prop.fName[0] == '@' 
+                ? char.ToUpper(prop.fName[1]) + prop.fName.Substring(2)
+                : char.ToUpper(prop.fName[0]) + prop.fName.Substring(1);
+            sb.AppendLine($"\t\tpublic {prop.fType} {prop.fName} {{get; set;}}");
         }
 
         // constructor
+        sb.AppendLine($"\t\tpublic {className} ({fieldsList}) {{");
         foreach(var f in fields){
             var fName = f.Split(' ', StringSplitOptions.RemoveEmptyEntries)[1];
-            sb.AppendLine($"\t\t{char.ToUpper(fName[0])}{fName.Substring(1)} = {fName}");
+            var propName = fName[0] == '@'
+                ? char.ToUpper(fName[1]) + fName.Substring(2)
+                : char.ToUpper(fName[0]) + fName.Substring(1);
+            sb.AppendLine($"\t\t\t{propName} = {fName};");
         }
+        sb.AppendLine("\t\t}");        
         sb.AppendLine("\t}");
+        sb.AppendLine();
     }
 }
